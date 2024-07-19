77ae42af12300355121908167bc7c6e62ebb9fdf
윤진 <151013932+maruru301@users.noreply.github.com>
2024-07-20 03:50:01 +0900

    setting: file structure에 따른 파일 생성

--- gomintapa/test/widget_test.dart
+++ gomintapa/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:gomintapa/main.dart';
+import 'package:gomintapa/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
