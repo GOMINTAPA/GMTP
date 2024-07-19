04319ded35e7286858831f6ee6c5ea026f419713
윤진 <151013932+maruru301@users.noreply.github.com>
2024-07-20 03:50:01 +0900

    setting: file structure에 따른 파일 생성 #1

---
+++ gomintapa/lib/src/screens/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Title'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.account_box_rounded),
+          ),
+        ],
+      ),
+    );
+  }
+}
