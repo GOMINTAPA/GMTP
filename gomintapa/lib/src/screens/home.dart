0b2b8363945a89254c833396a22fa88109fabac0
윤진 <151013932+maruru301@users.noreply.github.com>
2024-07-20 22:28:29 +0900

    comment: 주석 추가 #3

--- gomintapa/lib/src/screens/home.dart
+++ gomintapa/lib/src/screens/home.dart
@@ -3,6 +3,7 @@ import '../widgets/navigation/bottom_navigation_bar.dart';
 
 import '../widgets/navigation/app_bar.dart';
 
+// 화면에 표시할 위젯 목록
 final List<Widget> myTabItems = [
   Center(child: Text('막상막하')),
   Center(child: Text('홈')),
@@ -17,12 +18,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  // 현재 선택된 인덱스
   int _selectedIndex = 1;
   PageController _pageController = PageController(initialPage: 1);
 
+  // 바텀 네비게이션 바의 아이템이 클릭되었을 때
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
+      // 페이지 전환 애니메이션
       _pageController.animateToPage(index,
           duration: Duration(milliseconds: 300), curve: Curves.easeInOut);
     });
@@ -30,6 +34,7 @@ class _HomeState extends State<Home> {
 
   @override
   void dispose() {
+    // PageController의 리소스 해제
     _pageController.dispose();
     super.dispose();
   }
@@ -37,19 +42,19 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget(),
+      appBar: AppBarWidget(), // 상단 앱 바
       body: PageView(
-        controller: _pageController,
+        controller: _pageController, // 페이지 전환을 위한 컨트롤러
         onPageChanged: (index) {
           setState(() {
-            _selectedIndex = index;
+            _selectedIndex = index; // 페이지 변경 시 인덱스 업데이트
           });
         },
-        children: myTabItems,
+        children: myTabItems, // 페이지 뷰에 표시할 위젯 목록
       ),
       bottomNavigationBar: BottomNavigationBarWidget(
-        selectedIndex: _selectedIndex,
-        onItemTapped: _onItemTapped,
+        selectedIndex: _selectedIndex, // 현재 선택된 인덱스
+        onItemTapped: _onItemTapped, // 아이템 클릭 시 호출
       ),
     );
   }
--- gomintapa/lib/src/widgets/navigation/app_bar.dart
+++ gomintapa/lib/src/widgets/navigation/app_bar.dart
@@ -7,15 +7,15 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      centerTitle: true,
+      centerTitle: true, // title 중앙 배치
       title: SizedBox(
-        height: 60.0,
+        height: 60.0, // title 높이 설정
         child: Image.asset(
           'assets/images/gomintapa_logo.png',
-          fit: BoxFit.contain,
+          fit: BoxFit.contain, // 이미지의 비율을 유지하며 컨테이너 내에서 적절하게 조정
         ),
       ),
-      backgroundColor: Color(0xffD9D9D9),
+      backgroundColor: Color(0xffD9D9D9), // 상단바 배경색
       actions: [
         IconButton(
           onPressed: () {
@@ -23,14 +23,14 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
           },
           icon: Icon(Icons.search, size: 30),
         ),
-        SizedBox(width: 10),
+        SizedBox(width: 10), // 아이콘 버튼 사이 간격
         IconButton(
           onPressed: () {
             // 계정 버튼 클릭 시 동작
           },
           icon: Icon(Icons.account_box_rounded, size: 30),
         ),
-        SizedBox(width: 10),
+        SizedBox(width: 10), // 아이콘 버튼 사이 간격
       ],
     );
   }
