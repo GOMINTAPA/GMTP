88724292cea9529bb2a7b9bb16b87df630e2384d
윤진 <151013932+maruru301@users.noreply.github.com>
2024-07-20 23:53:13 +0900

    design: feedindex에 고민작성 버튼 생성 #3

--- gomintapa/lib/src/screens/feed/index.dart
+++ gomintapa/lib/src/screens/feed/index.dart
@@ -11,28 +11,57 @@ class _FeedIndexState extends State<FeedIndex> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.edit_outlined),
-      ),
-      body: Column(
+      body: Stack(
         children: [
-          Container(
-            color: Color(0xffD9D9D9),
-            height: 40,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                SizedBox(width: 5),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.filter_list_rounded,
-                    size: 25,
+          Column(
+            children: [
+              Container(
+                color: Color(0xffD9D9D9),
+                height: 40,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    SizedBox(width: 5),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.filter_list_rounded,
+                        size: 25,
+                      ),
+                    ),
+                    // 나중에 키워드 추가
+                  ],
+                ),
+              ),
+              // 다른 위젯이 여기 추가될 수 있음
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0), // 여백 추가
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  // 버튼 클릭 시 동작
+                },
+                icon: Icon(Icons.edit_outlined, color: Colors.red),
+                label: Text(
+                  '고민 작성',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                // 나중에 키워드 추가
-              ],
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white, // 배경색
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25), // 모서리 둥글기
+                  ),
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 20), // 패딩
+                ),
+              ),
             ),
           ),
         ],
