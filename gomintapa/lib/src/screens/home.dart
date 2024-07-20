04141218a7749c5939e5c9067e47a0cf235136c3
윤진 <151013932+maruru301@users.noreply.github.com>
2024-07-20 16:14:32 +0900

    style: 상단바에 앱 아이콘과 검색, 마이페이지 아이콘 추가 #3

--- gomintapa/lib/src/screens/home.dart
+++ gomintapa/lib/src/screens/home.dart
@@ -6,19 +6,37 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: false,
-        title: Text('Title'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.search),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60.0),
+        child: AppBar(
+          centerTitle: true,
+          title: SizedBox(
+            height: 60.0, // AppBar의 높이에 맞추기 위함
+            child: Image.asset(
+              'assets/images/gomintapa_logo.png',
+              fit: BoxFit.contain, // 이미지가 AppBar 안에 맞춰지도록
+            ),
           ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.account_box_rounded),
-          ),
-        ],
+          backgroundColor: Color(0xffD9D9D9),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                size: 30,
+              ),
+            ),
+            SizedBox(width: 10), // 아이콘 사이 여백
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.account_box_rounded,
+                size: 30,
+              ),
+            ),
+            SizedBox(width: 10), // 아이콘 사이 여백
+          ],
+        ),
       ),
     );
   }
