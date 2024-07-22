372ef3b193d7a540e74b5f398736bd4bd4f9ea14
윤진 <151013932+maruru301@users.noreply.github.com>
2024-07-23 05:39:04 +0900

    design: 사진 첨부하기 container 생성 #35

--- gomintapa/lib/src/screens/post/create_post.dart
+++ gomintapa/lib/src/screens/post/create_post.dart
@@ -16,6 +16,10 @@ class _CreatePostState extends State<CreatePost> {
 
   @override
   Widget build(BuildContext context) {
+    // 화면의 너비를 비율로 계산
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double containerWidth = screenWidth * 0.9; // 화면 너비의 90%로 설정
+
     return Scaffold(
       appBar: FormActionAppBar(),
       body: ListView(
@@ -78,6 +82,52 @@ class _CreatePostState extends State<CreatePost> {
                   keyboardType: TextInputType.multiline, // 멀티 라인 입력 설정
                 ),
                 SizedBox(height: 10),
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            padding: const EdgeInsets.only(
+              left: 40,
+              right: 40,
+              bottom: 40,
+              top: 10,
+            ),
+            child: Column(
+              children: [
+                Divider(
+                  thickness: 0.5,
+                  color: Color(0xffA7A7A7),
+                  height: 0, // Divider 위아래의 공간 제거
+                ),
+                SizedBox(height: 40),
+                // 화면 너비의 90%로 설정
+                Container(
+                  width: containerWidth, // 비율에 맞게 설정된 너비
+                  height: 180,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    border: Border.all(color: Color(0xff9B9B9B), width: 1),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.camera_alt_rounded,
+                        size: 30.0,
+                        color: Color(0xff9B9B9B),
+                      ),
+                      SizedBox(width: 10), // 아이콘과 텍스트 사이 간격
+                      Text(
+                        '사진 첨부하기',
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xff9B9B9B),
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
