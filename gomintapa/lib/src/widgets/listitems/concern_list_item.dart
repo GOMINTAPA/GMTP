9cf75202adac2faa8238faa91948f037f49cb5e3
로인 <151013952+bboroin@users.noreply.github.com>
2024-07-23 05:10:49 +0900

    design: ConcernListItem 위젯 생성 #37

---
+++ gomintapa/lib/src/widgets/listitems/concern_list_item.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ConcernListItem extends StatelessWidget {
+  const ConcernListItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // 제목 영역
+          // 선택 항목 영역
+        ],
+      ),
+    );
+  }
+}
