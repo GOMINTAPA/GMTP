04b92e4eed9309c8f77427688a9b23cb9619e50d
로인 <aroyne@naver.com>
2024-07-22 21:49:34 +0900

    design: 고민 정보 영역 탭 구현 #20

--- gomintapa/lib/src/screens/my/mypage.dart
+++ gomintapa/lib/src/screens/my/mypage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gomintapa/src/models/user_model.dart';
 import 'package:gomintapa/src/widgets/navigation/back_app_bar.dart';
+import 'package:gomintapa/src/widgets/sections/my/concern_feed_section.dart';
 import 'package:gomintapa/src/widgets/sections/my/user_section.dart';
 
 class MyPage extends StatefulWidget {
@@ -22,6 +23,9 @@ class _MyPageState extends State<MyPage> {
           UserSection(UserModel(id: 1, name: '노는게젤조아')),
           const SizedBox(height: 25),
           // 고민 정보 영역
+          Expanded(
+            child: ConcernFeedSection(),
+          ),
         ],
       ),
     );
