d59895628f6b5344a3a1c8a5e5b32eab3b92e750
윤진 <151013932+maruru301@users.noreply.github.com>
2024-07-24 04:19:57 +0900

    design: 선택 항목 섹션 생성 및 임의로 데이터 넣어서 확인 #32

--- gomintapa/lib/src/screens/post/post_detail.dart
+++ gomintapa/lib/src/screens/post/post_detail.dart
@@ -7,6 +7,15 @@ class PostDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 예시 데이터
+    final String title = "WOWWOWWOW";
+    final String content =
+        "아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보";
+    final String choiceAText = "선택 항목 A 텍스트";
+    final String choiceBText = "선택 항목 B 텍스트";
+    final String imageAPath = 'assets/images/jjanggu.jpg';
+    final String imageBPath = 'assets/images/dog.jpg';
+
     return Scaffold(
       appBar: BackAppBar(),
       body: Container(
@@ -15,9 +24,9 @@ class PostDetail extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 20),
+              SizedBox(height: 20),
               Padding(
-                padding: EdgeInsets.symmetric(
+                padding: const EdgeInsets.symmetric(
                   vertical: 20,
                   horizontal: 40,
                 ),
@@ -29,12 +38,36 @@ class PostDetail extends StatelessWidget {
                     Divider(color: Color(0xffD9D9D9)),
                     // 고민 내용(제목, 내용) 섹션
                     ContentSection(
-                      title: '안녕하세요ㅎㅎ',
-                      content:
-                          '아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 아로인 바보 ',
+                      title: title,
+                      content: content,
                     ),
                     Divider(color: Color(0xffD9D9D9)),
-                    // 항목, 사진 섹션
+                    // 선택 항목 섹션(2가지)
+                    // A 섹션
+                    ChoicesSection(
+                      choiceText: choiceAText,
+                      imagePath: imageAPath,
+                      backgroundColor: Color(0xffFF9B9B),
+                    ),
+                    // VS Text
+                    const Center(
+                      child: Text(
+                        'vs',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xff9B9B9B),
+                        ),
+                      ),
+                    ),
+                    // B 섹션
+                    ChoicesSection(
+                      choiceText: choiceBText,
+                      imagePath: imageBPath,
+                      backgroundColor: Color(0xff5DB1FF),
+                    ),
+                    SizedBox(height: 10),
+                    Divider(color: Color(0xffD9D9D9)),
                   ],
                 ),
               ),
@@ -66,7 +99,7 @@ class ProfileSection extends StatelessWidget {
         Text(
           'username', // 실제 사용자 이름으로 대체
           style: TextStyle(
-            fontSize: 15,
+            fontSize: 16,
             fontWeight: FontWeight.bold,
           ),
         ),
@@ -104,11 +137,114 @@ class ContentSection extends StatelessWidget {
           Text(
             content,
             style: const TextStyle(
-              fontSize: 16,
+              fontSize: 15,
             ),
           ),
           const SizedBox(height: 10),
         ],
+      ),
+    );
+  }
+}
+
+class ChoicesSection extends StatelessWidget {
+  final String choiceText;
+  final String imagePath;
+  final Color backgroundColor;
+
+  const ChoicesSection({
+    Key? key,
+    required this.choiceText,
+    required this.imagePath,
+    required this.backgroundColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 10),
+          PhotoDisplay(imagePath: imagePath),
+          TextDisplay(
+            backgroundColor: backgroundColor,
+            choiceText: choiceText,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PhotoDisplay extends StatelessWidget {
+  final String imagePath;
+
+  const PhotoDisplay({
+    Key? key,
+    required this.imagePath,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 180,
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        border: Border.all(color: const Color(0xff9B9B9B), width: 1),
+      ),
+      child: Image.asset(
+        imagePath,
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+}
+
+class TextDisplay extends StatelessWidget {
+  final Color backgroundColor;
+  final String choiceText;
+
+  const TextDisplay({
+    Key? key,
+    required this.backgroundColor,
+    required this.choiceText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        color: backgroundColor,
+        border: const Border(
+          bottom: BorderSide(
+            color: Color(0xff9B9B9B),
+            width: 1,
+          ),
+          left: BorderSide(
+            color: Color(0xff9B9B9B),
+            width: 1,
+          ),
+          right: BorderSide(
+            color: Color(0xff9B9B9B),
+            width: 1,
+          ),
+        ),
+      ),
+      child: Center(
+        child: Text(
+          choiceText,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
