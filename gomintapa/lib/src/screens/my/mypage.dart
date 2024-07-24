c5b63c0de27228e54e9c6b663154ceda24a14263
로인 <151013952+bboroin@users.noreply.github.com>
2024-07-24 17:38:51 +0900

    feat: 마이페이지 데이터 출력 #65

--- gomintapa/lib/src/screens/my/mypage.dart
+++ gomintapa/lib/src/screens/my/mypage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:gomintapa/src/controllers/user_controller.dart';
 import 'package:gomintapa/src/models/user_model.dart';
 import 'package:gomintapa/src/widgets/buttons/logout.button.dart';
 import 'package:gomintapa/src/widgets/navigation/back_app_bar.dart';
@@ -16,13 +18,23 @@ class MyPage extends StatefulWidget {
 class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
+    final userController = Get.put(UserController());
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: BackAppBar(),
       body: Column(
         children: [
           // 계정 정보 영역
-          UserSection(UserModel(id: 1, name: '노는게젤조아')),
+          //UserSection(UserModel(id: 1, name: '노는게젤조아')),
+          Obx(
+            () {
+              if (userController.my.value == null) {
+                return const CircularProgressIndicator();
+              } else {
+                return UserSection(userController.my.value!);
+              }
+            },
+          ),
           const SizedBox(height: 25),
           // 고민 정보 영역
           const Expanded(
