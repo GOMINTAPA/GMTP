4226765954bfaa7ced87092e80f93c1c548de606
로인 <151013952+bboroin@users.noreply.github.com>
2024-07-24 20:18:32 +0900

    fix: 마이페이지 회원에러 해결 #65

--- gomintapa/lib/src/providers/user_provider.dart
+++ gomintapa/lib/src/providers/user_provider.dart
@@ -1,20 +1,22 @@
-import 'dart:convert'; // JSON 디코드를 위해 추가
-import 'package:get/get.dart'; // HTTP 클라이언트 라이브러리를 위해 추가
+import 'package:get/get.dart';
 import 'provider.dart';
 
 class UserProvider extends Provider {
   Future<Map<String, dynamic>> show() async {
     try {
       final response = await get('/api/user/my');
+      print("Request URL: /api/user/my");
+      print("Response status: ${response.statusCode}");
+      print("Response body: ${response.body}");
+
       if (response.statusCode == 200) {
-        print("Response body: ${response.body}"); // 응답 본문 로깅
-        return json.decode(response.body) as Map<String, dynamic>;
+        return response.body as Map<String, dynamic>;
       } else {
         throw Exception(
             'Failed to load user data. Status code: ${response.statusCode}');
       }
     } catch (e) {
-      print("Error fetching user data: $e"); // 에러 로깅
+      print("Error fetching user data: $e");
       return {
         'result': 'error',
         'message': e.toString(),
