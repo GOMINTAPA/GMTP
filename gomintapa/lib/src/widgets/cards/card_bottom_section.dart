e214d332bbba3300e9db7372813b7dcbddd1155b
로인 <151013952+bboroin@users.noreply.github.com>
2024-07-25 06:30:01 +0900

    design: CardBottomSection 수정 #101

--- gomintapa/lib/src/widgets/cards/card_bottom_section.dart
+++ gomintapa/lib/src/widgets/cards/card_bottom_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gomintapa/src/widgets/cards/trapezoid_option.dart';
 
 class CardBottomSection extends StatelessWidget {
   final String option1;
@@ -15,41 +16,22 @@ class CardBottomSection extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        _buildOptionContainer(option1, Color.fromARGB(255, 255, 155, 155)),
-        const SizedBox(width: 10),
-        _buildOptionContainer(option2, Color.fromARGB(255, 93, 177, 255)),
-      ],
-    );
-  }
-
-  Widget _buildOptionContainer(String text, Color color) {
-    return Container(
-      width: 135,
-      height: 88,
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(5),
-      ),
-      alignment: Alignment.center,
-      child: Container(
-        width: 120,
-        height: 60,
-        padding: const EdgeInsets.all(5),
-        alignment: Alignment.center,
-        child: Text(
-          text,
-          maxLines: 2,
-          overflow: TextOverflow.ellipsis,
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-            fontFamily: 'Roboto',
-            fontWeight: FontWeight.w700,
-          ),
-          softWrap: true,
+        TrapezoidOption(
+          text: option1,
+          color: Color.fromARGB(255, 255, 155, 155),
+          clipper: UpsideDownTrapezoidClipper(),
+          alignment: Alignment.centerLeft,
+          textAlign: TextAlign.left,
         ),
-      ),
+        const SizedBox(width: 10),
+        TrapezoidOption(
+          text: option2,
+          color: Color.fromARGB(255, 93, 177, 255),
+          clipper: BottomWideTrapezoidClipper(),
+          alignment: Alignment.centerRight,
+          textAlign: TextAlign.right,
+        ),
+      ],
     );
   }
 }
