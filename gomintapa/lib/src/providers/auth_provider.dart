02a8d8806a8598da52068ceecd2deb4b4010e0f9
JMJ <ghtntkdnsk@naver.com>
2024-07-24 18:53:54 +0900

    리팩토링 완료

--- gomintapa/lib/src/providers/auth_provider.dart
+++ gomintapa/lib/src/providers/auth_provider.dart
@@ -2,15 +2,6 @@ import 'package:gomintapa/src/providers/provider.dart';
 
 class AuthProvider extends Provider {
 
-  Future<Map> requestPhoneCode(String phone) async {
-    final response = await post('/auth/phone', {'phone': phone});
-    return response.body;
-  }
-
-  Future<Map> verifyPhoneNumber(String code) async {
-    final response = await put('/auth/phone', {'code': code});
-    return response.body;
-  }
   Future<Map> register(String id, String password, String name) async {
     final response = await post('/auth/register', {
     'identifier': id,
